a69552d29c05b5e117eda11f00ac90ba6603d703
Ali Boudjemai <alboudje@z4r4p3.42lyon.fr>
2025-03-12 14:43:07 +0100

    Add input handle

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 all: build
 	# ./dist-newstyle/build/x86_64-linux/ghc-9.4.8/ft-turing-bis-0.1.0.0/x/ft-turing-bis/build/ft-turing-bis/ft-turing-bis machineExample/unary_sub.json 111-11= 
-	cabal run exes machineExample/unary_sub.json 111
+	cabal run exes machineExample/unary_sub.json 111-11=
 
 build:
 	cabal build
--- app/Main.hs
+++ app/Main.hs
@@ -20,19 +20,16 @@ main = do
             parseData <- parseFile jsonFilePath input
             -- | Appeler la machine de Turing ici. avec pasedData (StateMachine) et input (String)
             case parseData of
-                Left err -> putStrLn ("Error: " ++ err)
+                Left err -> putStrLn "Error: " ++ err
                 Right sm -> do
                     displayMachineData sm
                     let tr = getTransisionFromName (initial sm) sm
                     case tr of
                         Just transitions -> do
-                            -- let basic_list_pos = [(blank sm), (blank sm)..]
                             let basic_list_pos = [(head (blank sm)), (head (blank sm))..]
                             let basic_list_neg = [(head (blank sm)), (head (blank sm))..]
-                            let max_ellements = 7
-                            -- let max_ellements = 4
-                            let turing_sequence = ['1', '1', '1', '-', '1', '1', '='] ++ basic_list_pos
-                            -- let turing_sequence = ["1", "1", "1", "1"] ++ basic_list_pos
+                            let max_ellements = (length input)
+                            let turing_sequence = input ++ basic_list_pos
                             runTuringMachine turing_sequence basic_list_neg sm transitions 0 (initial sm) max_ellements (blank sm)
                             putStrLn "End"
                         Nothing -> do
