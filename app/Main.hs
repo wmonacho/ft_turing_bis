4bfe8ab495289cb1d62b0fc8ca172cfac42e6a23
William MONACHON <w.monachon@plan-c.io>
2025-03-12 13:28:39 +0100

    [TRG-P-04] fix merge parse + machine ()

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.Environment (getArgs)
 import Parser (parseFile)
+import StateMachine (StateMachine, blank)
 
 main :: IO ()
 main = do
@@ -10,9 +11,15 @@ main = do
         ["-h"] -> putStrLn usage
         ["--help"] -> putStrLn usage
         [jsonFilePath, input] -> do
-            parsedData <- parseFile jsonFilePath input
-            -- | Appeler la machine de Turing ici. avec pasedData (StateMachine) et input (String)
-            return ()
+            result <- parseFile jsonFilePath input
+            case result of
+                Left err -> putStrLn $ "Error: " ++ err
+                Right stateMachine -> do
+                    -- Appeler la machine de Turing ici avec stateMachine (StateMachine) et input (String)
+                    print (blank stateMachine)
+                    putStrLn "StateMachine parsed successfully"
+                    -- Ajoutez ici le code pour utiliser stateMachine et input
+                    return ()
         _ -> putStrLn usage
   where
     usage = "Usage: ft_turing [-h] jsonfile input\n\n" ++
